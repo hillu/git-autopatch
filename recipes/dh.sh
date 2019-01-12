e6bba66d92e933fafa5659c740903b459b7f421a
Hilko Bengen <bengen@hilluzination.de>
2019-01-12 14:59:12 +0100

    Use Debhelper 12

--- recipes/dh.sh
+++ recipes/dh.sh
@@ -1,10 +1,10 @@
 DETECT() {
-    test $(cat debian/compat) != 11
+    test $(cat debian/compat) != 12
 }
 
 PATCH() {
-    echo 11 > debian/compat
-    sed -i -e 's/debhelper\( *(.*)\)\?/debhelper (>= 11)/' debian/control
+    echo 12 > debian/compat
+    sed -i -e 's/debhelper\( *(.*)\)\?/debhelper (>= 12)/' debian/control
 }
 
 MESSAGE="Bump Debhelper compat level"
